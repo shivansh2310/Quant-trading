a732a60a5660ee48dc269ebbf7508015a7a9a739
shivansh2310 <42717219+shivansh2310@users.noreply.github.com>
2024-02-03 23:44:33 +0530

    Random Alpha Unit Final

--- main.py
+++ main.py
@@ -93,5 +93,10 @@ from utils import Alpha
 period_start = datetime(2010,1,1, tzinfo=pytz.UTC)
 period_end = datetime.now(pytz.utc)
 tickers, ticker_dfs = get_ticker_dfs(start=period_start, end=period_end)
+testfor = 20
+tickers = tickers[:testfor]
+
+
 alpha = Alpha(insts=tickers,dfs=ticker_dfs,start=period_start, end=period_end)
-alpha.run_simulation()
+portfolio_df = alpha.run_simulation()
+print(portfolio_df)
