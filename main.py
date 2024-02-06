c22a8b769cf036418f5bfb4fa84f865dad12919b
shivansh2310 <42717219+shivansh2310@users.noreply.github.com>
2024-02-07 00:06:13 +0530

    Alpha 1

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ tickers, ticker_dfs = get_ticker_dfs(start=period_start, end=period_end)
 testfor = 20
 tickers = tickers[:testfor]
 
-
-alpha = Alpha(insts=tickers,dfs=ticker_dfs,start=period_start, end=period_end)
-portfolio_df = alpha.run_simulation()
-print(portfolio_df)
+from alpha1 import Alpha1
+alpha1 = Alpha1(insts=tickers,dfs=ticker_dfs,start=period_start, end=period_end)
+df1 = alpha1.run_simulation()
+print(df1)
